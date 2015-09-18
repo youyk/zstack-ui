99b35077be9e1ef5261dad3d00f0cffe9d522938
Shawn Gao <shawngao5@gmail.com>
2015-09-18 15:57:01 +0800

    add bootstrap

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('zstackUI', [
   'ngRoute',
+  'ui.bootstrap',
   'zstackUI.login',
   'zstackUI.vminstance',
   'zstackUI.vmdetails',
