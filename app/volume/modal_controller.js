97f9c5a1ccdf4676451e889b1a10d9f4bccbbdac
BooguNote <boogunote@gmail.com>
2015-09-29 16:13:38 +0800

    add create image modal.

--- app/volume/modal_controller.js
+++ app/volume/modal_controller.js
@@ -91,3 +91,100 @@ angular.module('zstackUI.volume.modal.controller',
     $modalInstance.dismiss('cancel');
   };
 }])
+
+.controller('CreateImageModalCtrl', function ($scope, $modal, $log) {
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: false,
+      templateUrl: 'volume/create_image.html',
+      controller: 'CreateImageModalInstanceCtrl',
+      backdrop: 'static',
+      size: size,
+      resolve: {
+        modalScope: function () {
+          return $scope;
+        }
+      }
+    });
+
+    modalInstance.result.then(function () {
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+})
+
+.controller('CreateImageModalInstanceCtrl', ['$scope', 'ZStackApi', 'ZStackUtil', '$modalInstance', 'modalScope', function ($scope, ZStackApi, ZStackUtil, $modalInstance, modalScope) {
+  var self = $scope;
+  $scope.showDialog = true;
+
+  $scope.platformList = [
+                          'Linux',
+                          'Windows',
+                          'Other',
+                          'Paravirtualization'
+                        ];
+  $scope.bitsList = ['32', '64'];
+
+  $scope.system = "";
+  $scope.name = "";
+  $scope.description = "";
+  $scope.guestOsType = "";
+  $scope.platform = $scope.platformList[0];
+  $scope.bits = $scope.bitsList[0];
+  $scope.backupStorageList = [ZStackApi.defaultBackupStorage.uuid];
+
+  $scope.$on("child-dialog:open", function() {
+    $scope.showDialog = false;
+  })
+
+  $scope.$on("child-dialog:close", function(_, msg) {
+    console.log(msg)
+    $scope.showDialog = true;
+    if (ZStackUtil.notNullnotUndefined(msg))
+      $scope[msg.name] = msg.data;
+  })
+
+  $scope.select = function(name, value) {
+    console.log("select:"+name+":"+value)
+    $scope.safeApply(function() {
+      $scope[name] = value;
+    });
+  }
+
+  $scope.action = function(cb) {
+    var systemTags = [];
+    var msg = {
+      'org.zstack.header.image.APICreateRootVolumeTemplateFromRootVolumeMsg': {
+        name: self.name,
+        description: self.description,
+        guestOsType: self.guestOsType,
+        platform: self.platform,
+        backupStorageUuids: self.backupStorageList,
+        rootVolumeUuid: modalScope.$parent.data.uuid
+      }
+    }
+
+
+    ZStackApi.call(msg, function(data) {
+      console.log(data)
+      if (ZStackUtil.notNullnotUndefined(cb))
+        cb(data);
+    })
+  }
+  
+  $scope.ok = function() {
+    self.action(function(ret) {
+      console.log(ret)
+      modalScope.$emit("update:list");
+    })
+    $modalInstance.close('ok');
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+}])
